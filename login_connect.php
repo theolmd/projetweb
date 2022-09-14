2ae39cc9584dc87ed2d6d23d1ddcd9baca48a403
melinaaaa0805 <112492730+melinaaaa0805@users.noreply.github.com>
2022-09-15 00:16:45 +0200

    Add files via upload

--- login_connect.php
+++ login_connect.php
@@ -1,5 +1,6 @@
 <html>
 <head>
+    <link rel="stylesheet" type="text/css" href="CSS_login.css" />
     <link rel="stylesheet" type="text/css" href="Style.css" />
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -7,6 +8,18 @@
     <!--<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">-->
     <link rel="stylesheet" href="css/bootstrap-5.1.3-dist/css/bootstrap.css"/>
     <script src="css/bootstrap-5.1.3-dist/js/bootstrap.js"></script>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+    <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/jquery.validation/1.15.1/jquery.validate.min.js"></script>
+    <link href="https://fonts.googleapis.com/css?family=Kaushan+Script" rel="stylesheet">
+    <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet"
+          integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+
+    <!------ Include the above in your HEAD tag ---------->
+    <!--Fontawesome CDN-->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
 
     <!-- Titre de l'onglet -->
     <title>
@@ -19,28 +32,97 @@ include("header.html")
 
 <!-- Page de connexion -->
 
+<body>
+<div class="container">
+    <div class="row">
+        <div class="col-md-5 mx-auto">
+            <div id="first">
+                <div class="myform form ">
+                    <div class="logo mb-3">
+                        <div class="col-md-12 text-center">
+                            <h1>Se connecter</h1>
+                        </div>
+                    </div>
+                    <form action="profile.php" method="POST">
+                        <div class="form-group">
+                            <label for="mail">Adresse mail</label>
+                            <input type="email" name="mail"  class="form-control"
+                                   aria-describedby="emailHelp" placeholder="Entrer votre adresse mail">
+                        </div>
+                        <div class="form-group">
+                            <label for="mdp">Mot de passe</label>
+                            <input type="password" name="mdp"  class="form-control"
+                                   aria-describedby="emailHelp" placeholder="Entrer votre mot de passe">
+                        </div>
+                        <div class="form-group">
+                            <p class="text-center">En vous connectant vous acceptez les
+                                <a href="#">Conditions d'utilisation</a></p>
+                        </div>
+                        <div class="col-md-12 text-center ">
+                            <button type="submit" class=" btn btn-block mybtn btn-primary tx-tfm" name="login"
+                            value="login" id="login">Se connecter</button>
+                        </div>
+                        <div class="col-md-12 ">
+                            <div class="login-or">
+                                <hr class="hr-or">
+                                <span class="span-or">ou</span>
+                            </div>
+                        </div>
+                        <div class="col-md-12 mb-3">
+                            <p class="text-center">
+                                <a href="javascript:void();" class="google btn mybtn"><i class="fa fa-google-plus">
+                                    </i> S'enregistrer avec Google
+                                </a>
+                            </p>
+                        </div>
+                        <div class="form-group">
+                            <p class="text-center"> Pas de compte ? <a href="login_register.php" id="signup">
+                                    Enregistrez-vous ici</a></p>
+                        </div>
+                    </form>
 
-    <body>
-
-    <div class="container">
-        <form action ="profile.php" method="POST">
-        <h1>Se connecter</h1>
-            <div class ="">
-        <label for="email" class="form-label"><b>Email</b></label>
-        <input type="text" class="form-control" placeholder="Entrez l'adresse mail" name="mail" required>
-    </div>
-            <div class="">
-        <label for="psw" class="form-label"><b>Mot de passe</b></label>
-        <input type="password" class="form-control" placeholder="Entrez le mot de passe" name="mdp" required>
-    </div>
-            <div class="row">
-        <button onclick=verif() type="submit" name="login" value ="login" id="login">Se connecter</button>
+                </div>
+            </div>
+            <div id="second">
+                <div class="myform form ">
+                    <div class="logo mb-3">
+                        <div class="col-md-12 text-center">
+                            <h1 >Signup</h1>
+                        </div>
+                    </div>
+                    <form action="#" name="registration">
+                        <div class="form-group">
+                            <label for="exampleInputEmail1">First Name</label>
+                            <input type="text"  name="firstname" class="form-control" id="firstname" aria-describedby="emailHelp" placeholder="Enter Firstname">
+                        </div>
+                        <div class="form-group">
+                            <label for="exampleInputEmail1">Last Name</label>
+                            <input type="text"  name="lastname" class="form-control" id="lastname" aria-describedby="emailHelp" placeholder="Enter Lastname">
+                        </div>
+                        <div class="form-group">
+                            <label for="exampleInputEmail1">Email address</label>
+                            <input type="email" name="email"  class="form-control" id="email" aria-describedby="emailHelp" placeholder="Enter email">
+                        </div>
+                        <div class="form-group">
+                            <label for="exampleInputEmail1">Password</label>
+                            <input type="password" name="password" id="password"  class="form-control" aria-describedby="emailHelp" placeholder="Enter Password">
+                        </div>
+                        <div class="col-md-12 text-center mb-3">
+                            <button type="submit" class=" btn btn-block mybtn btn-primary tx-tfm">Get Started For Free</button>
+                        </div>
+                        <div class="col-md-12 ">
+                            <div class="form-group">
+                                <p class="text-center"><a href="#" id="signin">Already have an account?</a></p>
+                            </div>
+                        </div>
+                </div>
+                </form>
+            </div>
         </div>
-            <label><a href="login_register.php">Pas de compte ? S'inscrire.</a></label>
     </div>
-    </form>
+</div>
 
-    </body>
+</body>
 <script>
     function verif()
     //Affiche une alerte si au moins l'un des deux champs est vide
